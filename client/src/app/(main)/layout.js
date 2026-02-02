9b2d3a880c11d26c0772303a9c75551677a080df
Pangkeek <supawatkhaithong@gmail.com>
2026-02-03 00:26:24 +0700

    select

--- client/src/app/(main)/layout.js
+++ client/src/app/(main)/layout.js
@@ -11,14 +11,16 @@ const spmono = Space_Mono({
 function Mainlayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#141720] px-4">
-        <nav className='w-full flex justify-between px-22 py-16'>
+        <nav className='w-full flex justify-between px-22 py-10'>
             <div className='w-[520px] flex justify-between items-center'>
                 <a className={`${spmono.className} font-bold text-white text-[20px]`}>morse<span className='text-[#EF4444]'>code</span></a>
                 <a className={`${spmono.className} font-bold text-[#9CA3AF]`}>practice</a>
                 <a className={`${spmono.className} font-bold text-[#9CA3AF]`}>leaderboard</a>
                 <a className={`${spmono.className} font-bold text-[#9CA3AF]`}>about</a>
             </div>
-            <div className={`${spmono.className} w-12 h-12 bg-[#252B3D] rounded-full flex items-center justify-center font-bold text-[14px] text-white outline outline-[#EF4444]`}>jp</div>
+            <div className={`${spmono.className} w-12 h-12 bg-[#252B3D] rounded-full flex items-center justify-center font-bold text-[14px] text-white outline outline-[#EF4444]`}>
+              <p className='mb-1'>jp</p>
+            </div>
         </nav>
         <div className='w-full px-7'>
             <div className='bg-white w-full h-[1px]'></div>
--- client/src/app/(main)/page.js
+++ client/src/app/(main)/page.js
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import React from 'react'
+import { Space_Mono } from 'next/font/google';
+
+const spmono = Space_Mono({
+  subsets: ['latin'],
+  weight: ["400", "700"],
+})
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <div className={`${spmono.className} font-bold w-[705px] h-[65px] bg-[#1E2332] rounded-xl flex items-center justify-between mt-14`}>
+        <p className='text-[#9CA3AF] pl-10'>decode</p>
+        <p className='text-[#9CA3AF]'>encode</p>
+        <p className='text-[#9CA3AF]'>|</p>
+        <p className='text-[#9CA3AF]'>a-z</p>
+        <p className='text-[#9CA3AF]'>word</p>
+        <p className='text-[#9CA3AF]'>|</p>
+        <p className='text-[#9CA3AF]'>10</p>
+        <p className='text-[#9CA3AF]'>15</p>
+        <p className='text-[#9CA3AF]'>50</p>
+        <p className='text-[#9CA3AF] pr-10'>100</p>
+      </div>
     </div>
   );
 }
